69f219a61ac6ffbf2685799a5531b4f6ea2bacdb
Bartoszkk <Bartosz.michalak@epitech.eu>
2023-12-12 15:57:07 +0100

    ad

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+program_output=$(./test)
+
+# Define the expected output without a trailing newline
+expected_output="Hello World!"
+
+# Remove the trailing newline from the program output
+program_output_no_newline=$(echo -n "$program_output")
+
+# Compare the program output to the expected output
+if [ "$program_output_no_newline" = "$expected_output" ]; then
+  echo "Output matches the expected result!"
+else
+  echo "Output does not match the expected result."
+fi
