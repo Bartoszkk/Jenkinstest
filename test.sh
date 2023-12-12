54760395405f56177a001f24b0d0329777d4c62c
Bartoszkk <Bartosz.michalak@epitech.eu>
2023-12-12 15:58:55 +0100

    add

--- test.sh
+++ test.sh
@@ -11,8 +11,8 @@ program_output_no_newline=$(echo -n "$program_output")
 # Compare the program output to the expected output
 if [ "$program_output_no_newline" = "$expected_output" ]; then
   echo "Output matches the expected result!"
-  exit 1
+  exit 0
 else
   echo "Output does not match the expected result."
-  exit 0
+  exit 1
 fi
